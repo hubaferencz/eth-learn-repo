0f9a88641df8fc4f4bd7f5bb26dc37ea47373974
Huba Ferencz <huba.ferencz.personal@gmail.com>
2023-01-12 07:54:17 +0200

    solidity gotcha

--- firstContract/ballot.sol
+++ firstContract/ballot.sol
@@ -11,7 +11,7 @@ contract Inbox {
         message = newMessage;
     }
     
-    function getMessage() public view returns (string) {
-        return message;
-    }   
+    // function getMessage() public view returns (string) {
+    //     return message;
+    // }   
 }
